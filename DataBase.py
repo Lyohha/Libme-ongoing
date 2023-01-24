a3736625b98f28bb6afed693d48c7b9e1f4624a7
Lyohha <325alex.000@gnail.com>
2023-01-24 16:44:33 +0200

    Add base structure for database

--- DataBase.py
+++ DataBase.py
@@ -1,3 +1,5 @@
+import psycopg2
+import os
 
 
 def init():
@@ -7,4 +9,73 @@ def init():
 class DataBase:
 
     def __init__(self):
-        pass
+        self.__create_table_bot_settings = "CREATE TABLE IF NOT EXISTS bot_settings( " \
+                                           "key VARCHAR(100) NOT NULL," \
+                                           "value VARCHAR(100) NOT NULL);"
+
+        self.__create_table_links = "CREATE TABLE IF NOT EXISTS links(" \
+                                    "id SERIAL, " \
+                                    "link TEXT, " \
+                                    "last VARCHAR(30));"
+
+        self.__create_table_tg_user = "CREATE TABLE IF NOT EXISTS tg_users(" \
+                                      "chat_id VARCHAR(100), " \
+                                      "show_per_page INT DEFAULT '5', " \
+                                      "last_messages TEXT);"
+
+        self.__create_table_tg_user_feed = "CREATE TABLE IF NOT EXISTS tg_user_feed(" \
+                                           "id SERIAL, " \
+                                           "tg_user_id BIGINT NOT NULL, " \
+                                           "link_id BIGINT NOT NULL);"
+        self.__create_table_downloads = "CREATE TABLE IF NOT EXISTS tg_user_feed(" \
+                                        "id SERIAL, " \
+                                        "file_id BIGINT NOT NULL, " \
+                                        "item_id VARCHAR(100) NOT NULL, " \
+                                        "link_id BIGINT NOT NULL);"
+
+        self.__get_tg_user_by_chat_id = "SELECT * FROM tg_users WHERE chat_id = %s;"
+
+        self.__insert_tg_user = "INSERT INTO tg_users (chat_id, show_per_page) " \
+                                "VALUES (%s, %s);"
+
+        self.__update_tg_user = "UPDATE tg_users SET show_per_page = %s WHERE chat_id = %s;"
+
+        self.__update_tg_user_last_messages = "UPDATE tg_users SET last_messages = %s WHERE chat_id = %s;"
+
+        self.__get_all_links = "SELECT * FROM links;"
+
+    def __connection(self):
+        return psycopg2.connect(
+            host=os.getenv('HOST', default=None),
+            user=os.getenv('USER', default=None),
+            password=os.getenv('PASSWORD', default=None),
+            database=os.getenv('DATABASE', default=None),
+        )
+
+    def init_database(self):
+        connection = self.__connection()
+
+        with connection:
+            cur = connection.cursor()
+            cur.execute(self.__create_table_bot_settings)
+            cur.close()
+
+        with connection:
+            cur = connection.cursor()
+            cur.execute(self.__create_table_links)
+            cur.close()
+
+        with connection:
+            cur = connection.cursor()
+            cur.execute(self.__create_table_tg_user)
+            cur.close()
+
+        with connection:
+            cur = connection.cursor()
+            cur.execute(self.__create_table_tg_user_feed)
+            cur.close()
+
+        with connection:
+            cur = connection.cursor()
+            cur.execute(self.__create_table_downloads)
+            cur.close()
--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import DataBase
+from dotenv import load_dotenv
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+load_dotenv()
 
 
-# Press the green button in the gutter to run the script.
+def main():
+    DataBase.init().init_database()
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
